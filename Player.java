2a0c769f246942e977b37ef24dcfb75023c0a176
ArthurMace <159178119+ArthurMace@users.noreply.github.com>
2025-12-19 13:47:04 -0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,16 +3,12 @@ package elifoot;
 public class Player {
 
     String name;
-    String position;
     int attack;
     int defense;
-    int stamina;
 
-    public Player(String name, String position, int attack, int defense, int stamina) {
+    public Player(String name, int attack, int defense) {
         this.name = name;
-        this.position = position;
         this.attack = attack;
         this.defense = defense;
-        this.stamina = stamina;
     }
 }

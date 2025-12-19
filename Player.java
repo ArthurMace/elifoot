64eca2240130f10c876541610f6bf1cec03a074d
ArthurMace <159178119+ArthurMace@users.noreply.github.com>
2025-12-19 13:41:54 -0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,18 @@
+package elifoot;
+
+public class Player {
+
+    String name;
+    String position;
+    int attack;
+    int defense;
+    int stamina;
+
+    public Player(String name, String position, int attack, int defense, int stamina) {
+        this.name = name;
+        this.position = position;
+        this.attack = attack;
+        this.defense = defense;
+        this.stamina = stamina;
+    }
+}
